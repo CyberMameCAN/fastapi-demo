62a8d22058ee95eec48adcb89df15192bad6968b
CyberMameCAN <toshi@atbat.jp>
2022-04-11 16:41:28 +0900

    円周率を計算で求めるデモのコードをflake8修正

--- fastapi_demo/.ipynb_checkpoints/main-checkpoint.py
+++ fastapi_demo/.ipynb_checkpoints/main-checkpoint.py
@@ -1,4 +1,3 @@
-import pathlib
 from fastapi import FastAPI
 from pathlib import Path
 from .calculator import enshuritsu
@@ -7,18 +6,22 @@ BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.get("/enshuritsu/montecalro/{calc_count}")
 def read_montecalro(calc_count: int = 100):
     mypi = enshuritsu.montecarlo_method(calc_count)
     return {"PI=": mypi, "count": calc_count}
+
 
 @app.get("/enshuritsu/gregory_prog/{calc_end}")
 def read_gregory(calc_end: int = 1000):
